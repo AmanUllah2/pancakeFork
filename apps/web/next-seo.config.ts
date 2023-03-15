db070a187743c249c9501654088a345c8c1c85a8
Aman Ullah <amansheikh121@gmail.com>
2023-03-15 18:28:23 +0500

    changes of meta

--- apps/web/next-seo.config.ts
+++ apps/web/next-seo.config.ts
@@ -13,7 +13,7 @@ export const SEO: DefaultSeoProps = {
   openGraph: {
     title: '🥞 KingKongDoge - A next evolution DeFi exchange on BNB Smart Chain (BSC)',
     description:
-      'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by KingKongDoge), NFTs, and more, on a platform you can trust.',
-    images: [{ url: 'https://assets.pancakeswap.finance/web/og/hero.jpg' }],
+      'The most popular AMM on BSC by user count!',
+    // images: [{ url: 'https://assets.pancakeswap.finance/web/og/hero.jpg' }],
   },
 }
--- apps/web/src/config/constants/meta.ts
+++ apps/web/src/config/constants/meta.ts
@@ -6,8 +6,8 @@ import { ASSET_CDN } from './endpoints'
 export const DEFAULT_META: PageMeta = {
   title: 'KingKongDoge',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by KingKongDoge), NFTs, and more, on a platform you can trust.',
-  image: `https://i.ibb.co/HV1DFBX/Screenshot-2023-03-15-at-12-09-16-PM.png`,
+    'The most popular AMM on BSC by user count!',
+  // image: `https://i.ibb.co/HV1DFBX/Screenshot-2023-03-15-at-12-09-16-PM.png`,
 }
 
 interface PathList {
